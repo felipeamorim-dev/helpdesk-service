70b5c35732153e6df4d738677e19945196831c39
felipeamorim-dev <felipeamorim.dev@gmail.com>
2022-05-10 00:03:15 -0300

    :wrench: Configuração das propriedades da aplicação

---
+++ src/main/java/com/app/helpdesk/config/TestConfig.java
@@ -0,0 +1,11 @@
+package com.app.helpdesk.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+
+}
