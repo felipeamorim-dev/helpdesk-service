d762c84433d8b2a92d5e5329903976b60157ece9
felipeamorim-dev <felipeamorim.dev@gmail.com>
2022-05-18 08:58:10 -0300

    :sparkles: adicionando exceptions

---
+++ src/main/java/com/app/helpdesk/exceptions/DataIntegrityViolationException.java
@@ -0,0 +1,13 @@
+package com.app.helpdesk.exceptions;
+
+public class DataIntegrityViolationException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DataIntegrityViolationException(String message){
+        super(message);
+    }
+
+    public DataIntegrityViolationException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
