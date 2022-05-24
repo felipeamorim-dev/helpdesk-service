1203683167fce9dec7961be9680ac81439820f05
felipeamorim-dev <felipeamorim.dev@gmail.com>
2022-05-24 01:39:39 -0300

    :wrench: configuração para o model mapper da entidade tecnico

--- src/main/java/com/app/helpdesk/config/MapperConfig.java
+++ src/main/java/com/app/helpdesk/config/MapperConfig.java
@@ -2,6 +2,8 @@ package com.app.helpdesk.config;
 
 import com.app.helpdesk.domain.Cliente;
 import com.app.helpdesk.domain.Dto.ClienteDTO;
+import com.app.helpdesk.domain.Dto.TecnicoDTO;
+import com.app.helpdesk.domain.Tecnico;
 import com.app.helpdesk.domain.enums.Perfil;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
@@ -22,9 +24,11 @@ public class MapperConfig {
         // Realiza a converção da collection de perfil em uma collection de integer, dessa forma, é possível realizar a correspondência de métodos entre o DTO de clientes e Clientes.
         Converter<Set<Perfil>, Set<Integer>> perfilSetConverter = cxt -> cxt.getSource().stream().map(perfil -> perfil.getCodigo()).collect(Collectors.toSet());
         modelMapper.createTypeMap(ClienteDTO.class, Cliente.class).addMappings(mapper -> mapper.using(perfilSetConverter).map(ClienteDTO::getPerfis, Cliente::setPerfis));
+        modelMapper.createTypeMap(TecnicoDTO.class, Tecnico.class).addMappings(mapper -> mapper.using(perfilSetConverter).map(TecnicoDTO::getPerfis, Tecnico::setPerfis));
 
         // Configuração do mapeamento entidade clientes para realizar atualizações de dados da entidade.
         modelMapper.createTypeMap(Cliente.class, Cliente.class).addMappings(mapper -> mapper.using(perfilSetConverter).map(Cliente::getPerfis, Cliente::setPerfis));
+        modelMapper.createTypeMap(Tecnico.class, Tecnico.class).addMappings(mapper -> mapper.using(perfilSetConverter).map(Tecnico::getPerfis, Tecnico::setPerfis));
 
         return modelMapper;
     }
