2df302d04c1ec6f7619ac29c8adacf62f4fca532
felipeamorim-dev <felipeamorim.dev@gmail.com>
2022-05-24 01:38:55 -0300

    :sparkles: adicionando service e dto do tecnico

---
+++ src/main/java/com/app/helpdesk/domain/Dto/TecnicoDTO.java
@@ -0,0 +1,40 @@
+package com.app.helpdesk.domain.Dto;
+
+import com.app.helpdesk.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+public class TecnicoDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    protected Integer id;
+
+    @NotNull(message = "O campo NOME é requerido")
+    protected String nome;
+
+    @NotNull(message = "O campo EMAIL é requerido")
+    protected String email;
+
+    @CPF
+    @NotNull(message = "O campo CPF é requerido")
+    protected String cpf;
+
+    @NotNull(message = "O campo SENHA é requerido")
+    protected String senha;
+
+    protected Set<Perfil> perfis = new HashSet<>();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    protected LocalDate dataCriacao = LocalDate.now();
+
+}
---
+++ src/main/java/com/app/helpdesk/services/TecnicoServiceImpl.java
@@ -0,0 +1,79 @@
+package com.app.helpdesk.services;
+
+import com.app.helpdesk.domain.Cliente;
+import com.app.helpdesk.domain.Dto.ClienteDTO;
+import com.app.helpdesk.domain.Dto.TecnicoDTO;
+import com.app.helpdesk.domain.Pessoa;
+import com.app.helpdesk.domain.Tecnico;
+import com.app.helpdesk.domain.enums.Perfil;
+import com.app.helpdesk.exceptions.DataIntegrityViolationException;
+import com.app.helpdesk.exceptions.ObjectNotFoundException;
+import com.app.helpdesk.repositories.ClienteRepository;
+import com.app.helpdesk.repositories.PessoaRepository;
+import com.app.helpdesk.repositories.TecnicoRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TecnicoServiceImpl implements DefaultService<Tecnico, TecnicoDTO> {
+
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public Tecnico findById(Integer id) {
+        return tecnicoRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! ID: " + id));
+    }
+
+    @Override
+    public Tecnico create(TecnicoDTO entityDto) {
+        validaPorCpfEEmail(entityDto);
+        Tecnico tecnico = modelMapper.map(entityDto, Tecnico.class);
+        tecnico.addPerfil(Perfil.TECNICO);
+        return tecnicoRepository.save(tecnico);
+    }
+
+    @Override
+    public List<Tecnico> findAll() {
+        return tecnicoRepository.findAll();
+    }
+
+    @Override
+    public Tecnico update(Integer id, TecnicoDTO entityDto) {
+        entityDto.setId(id);
+        Tecnico tecnicoAtualizado = findById(id);
+        validaPorCpfEEmail(entityDto);
+        modelMapper.map(modelMapper.map(entityDto, Tecnico.class), tecnicoAtualizado);
+        return tecnicoRepository.save(tecnicoAtualizado);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        Tecnico obj = findById(id);
+        if (obj.getChamados().size() > 0) {
+            throw new DataIntegrityViolationException("Cliente possui ordens de serviço e não pode ser deletado!");
+        }
+
+        tecnicoRepository.deleteById(id);
+    }
+
+    private void validaPorCpfEEmail(TecnicoDTO entityDto) {
+        Optional<Pessoa> obj = pessoaRepository.findByCpf(entityDto.getCpf());
+        if (obj.isPresent() && obj.get().getId() != entityDto.getId())
+            throw new DataIntegrityViolationException("CPF já cadastrado no sistema!");
+
+        obj = pessoaRepository.findByEmail(entityDto.getEmail());
+        if (obj.isPresent() && obj.get().getId() != entityDto.getId())
+            throw new DataIntegrityViolationException("E-mail já cadastrado no sistema!");
+    }
+}
