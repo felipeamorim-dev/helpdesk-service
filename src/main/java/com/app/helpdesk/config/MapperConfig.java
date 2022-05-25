029571a7100b3bcd94f62a980f57e52eb7de269d
felipeamorim-dev <felipeamorim.dev@gmail.com>
2022-05-24 23:54:40 -0300

    :wrench: configuração do model mapper para converter chamado em chamadoDto

--- src/main/java/com/app/helpdesk/config/MapperConfig.java
+++ src/main/java/com/app/helpdesk/config/MapperConfig.java
@@ -1,10 +1,14 @@
 package com.app.helpdesk.config;
 
+import com.app.helpdesk.domain.Chamado;
 import com.app.helpdesk.domain.Cliente;
+import com.app.helpdesk.domain.Dto.ChamadoDTO;
 import com.app.helpdesk.domain.Dto.ClienteDTO;
 import com.app.helpdesk.domain.Dto.TecnicoDTO;
 import com.app.helpdesk.domain.Tecnico;
 import com.app.helpdesk.domain.enums.Perfil;
+import com.app.helpdesk.domain.enums.Prioridade;
+import com.app.helpdesk.domain.enums.Status;
 import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -29,6 +33,13 @@ public class MapperConfig {
         // Configuração do mapeamento entidade clientes para realizar atualizações de dados da entidade.
         modelMapper.createTypeMap(Cliente.class, Cliente.class).addMappings(mapper -> mapper.using(perfilSetConverter).map(Cliente::getPerfis, Cliente::setPerfis));
         modelMapper.createTypeMap(Tecnico.class, Tecnico.class).addMappings(mapper -> mapper.using(perfilSetConverter).map(Tecnico::getPerfis, Tecnico::setPerfis));
+
+        // Mapeamento para a converção de chamado para chamadoDto
+        Converter<Prioridade, Integer> prioridadeIntegerConverter = cxt -> cxt.getSource().getCodigo();
+        Converter<Status, Integer> statusIntegerConverter = cxt -> cxt.getSource().getCodigo();
+        modelMapper.createTypeMap(Chamado.class, ChamadoDTO.class)
+                .addMappings(mapper -> mapper.using(prioridadeIntegerConverter).map(Chamado::getPrioridade, ChamadoDTO::setPrioridade))
+                .addMappings(mapper -> mapper.using(statusIntegerConverter).map(Chamado::getStatus, ChamadoDTO::setStatus));
 
         return modelMapper;
     }
