f2ef5b2b9661d5858ec62dd2e24788ce5376b6db
felipeamorim-dev <felipeamorim.dev@gmail.com>
2022-05-24 23:55:12 -0300

    :wrench: modificação no parâmetro de extensão do defaultservice

--- src/main/java/com/app/helpdesk/services/DefaultService.java
+++ src/main/java/com/app/helpdesk/services/DefaultService.java
@@ -3,7 +3,7 @@ package com.app.helpdesk.services;
 import com.app.helpdesk.domain.Pessoa;
 import java.util.List;
 
-public interface DefaultService<E extends Pessoa, T> {
+public interface DefaultService<E extends Pessoa, T extends Object> {
 
     E findById(Integer id);
     E create(T entityDto);
