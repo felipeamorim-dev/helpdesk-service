4ed43d295020c483c1abd7976c1de0d437a450c7
felipeamorim-dev <felipeamorim.dev@gmail.com>
2022-05-24 22:14:45 -0300

    :sparkles: criado chamadoDTO e credenciaisDTO

---
+++ src/main/java/com/app/helpdesk/domain/Dto/ChamadoDTO.java
@@ -0,0 +1,46 @@
+package com.app.helpdesk.domain.Dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+public class ChamadoDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Integer id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataAbertura = LocalDate.now();
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataFechamento;
+
+    @NotNull(message = "O campo PRIORIDADE é requerido")
+    private Integer prioridade;
+
+    @NotNull(message = "O campo STATUS é requerido")
+    private Integer status;
+
+    @NotNull(message = "O campo TÍTULO é requerido")
+    private String titulo;
+
+    @NotNull(message = "O campo OBSERVAÇÕES é requerido")
+    private String observacoes;
+
+    @NotNull(message = "O campo TÉCNICO é requerido")
+    private Integer tecnicoId;
+
+    @NotNull(message = "O campo CLIENTE é requerido")
+    private Integer clienteId;
+
+    private String nomeTecnico;
+
+    private String nomeCliente;
+
+}
---
+++ src/main/java/com/app/helpdesk/domain/Dto/CredenciaisDTO.java
@@ -0,0 +1,10 @@
+package com.app.helpdesk.domain.Dto;
+
+import lombok.Data;
+
+@Data
+public class CredenciaisDTO {
+
+    private String email;
+    private String senha;
+}
